efedf12e97db49be15a0943bafbec20f0d266e61
Preston Bateman <prestonbbateman@gmail.com>
2022-09-19 12:48:32 -0600

    working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 portarray = [
     {
-        label: "Week 1 Notes",
-        url: "week1/index.html"
+        "label" : "Week 1 Code",
+        "url" : "week1/index.html"
+    },
+    {
+        "label" : "Week 2 Code",
+        "url" : "week2/index.html"
     }
 ];
-for (let i=0; i < portarrar.length; i++) {
+
+portarray.forEach(value => {
     let x = document.createElement("li");
-    x.innerText = "";
+    let text = value.label;
+    const att1 = document.createAttribute('id');
+    att1.value = value.label;
+    x.setAttributeNode(att1)
     document.getElementById("table-content").appendChild(x);
-}
+    let y = document.createElement("a");
+    let linkText = value.url;
+    const att = document.createAttribute("href");
+    att.value = linkText;
+    y.setAttributeNode(att);
+    y.innerText = text;
+    document.getElementById(value.label).appendChild(y);
+});
