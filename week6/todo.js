3399736baa917c91c6651eb39f2400107ebecb9a
Preston Bateman <prestonbbateman@gmail.com>
2022-10-21 16:35:12 -0600

    midterm initial

---
+++ week6/todo.js
@@ -0,0 +1,87 @@
+import * as Utilities from './utilities.js'
+
+
+//checking if there is already data
+if (localStorage.getItem('data') === null){
+    localStorage.setItem('data', '[]')
+}
+const taskArray = JSON.parse(localStorage.getItem('data'));
+
+const btn = document.getElementById("btn");
+let tasksLeft = document.getElementById("tasks-left");
+tasksLeft.innerHTML = 0;
+
+Utilities.createList1();
+
+function updateBoard () {
+    updateTask()
+    Utilities.updateView()
+    clearValue()
+}
+
+function updateTask () {
+    const content = document.getElementById("entered-text").value.trim();
+    const newTask = new Utilities.Task(content);
+    taskArray.push(newTask);
+    const arrayString = JSON.stringify(taskArray);
+    localStorage.setItem('data', arrayString);
+}
+
+
+function clearValue () {
+    const el = document.getElementById("entered-text");
+    el.value = ""
+}
+
+// listener for the button click
+document.getElementById("btn").addEventListener('click', updateBoard);
+
+function updatelist(ind) {
+    const obj = JSON.parse(localStorage.getItem('data'));
+    obj[ind].completed = !obj[ind].completed;
+    localStorage.setItem('data', JSON.stringify(obj));
+    updateView();
+}
+
+
+window.updatelist = updatelist;
+window.updateView = Utilities.updateView;
+window.deleteRow = Utilities.deleteRow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+}
+
+
+
+
---
+++ week6/utilities.js
@@ -0,0 +1,109 @@
+export const Task = function (content) {
+    this.id = new Date();
+    this.content = content;
+    this.completed = false;
+}
+
+export function createList () {
+    const obj = JSON.parse(localStorage.getItem('data'))
+    obj.forEach(value => {
+    let x = document.createElement("div");
+    let text = value.content;
+    const att1 = document.createAttribute('class');
+    att1.value = 'task-list';
+    const att2 = document.createAttribute('id');
+    att2.value = value.id;
+    x.setAttributeNode(att1);
+    x.setAttributeNode(att2);
+    x.innerText = text;
+    console.log(x);
+    document.getElementById("todo-list").appendChild(x);
+    });
+
+}
+
+export function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+function createCheck(att2) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add(att2, 'check-box');
+    newDiv.innerText = "X"
+    const divID = document.createAttribute('id');
+}
+
+function createDelete(att2) {
+    let dBTN = document.createElement('div');
+    dBTN.classList.add(att2, 'delete-button');
+    dBTN.innerText = 'X';
+}
+
+function createTaskInfo(text, att2) {
+    let newTaskItem = document.createElement('div');
+    newTaskItem.classList.add(att2, 'task-text');
+    newTaskItem.innerText = text;
+}
+
+export function updatelist(ind) {
+    const obj = JSON.parse(localStorage.getItem('data'));
+    obj[ind].completed = !obj[ind].completed;
+    localStorage.setItem('data', JSON.stringify(obj));
+    updateView();
+}
+
+export function deleteRow (ind) {
+    const obj = JSON.parse(localStorage.getItem('data'));
+    obj.splice(ind, 1);
+    localStorage.setItem('data', JSON.stringify(obj));
+    updateView();
+}
+
+export function createList1 () {
+    const obj = JSON.parse(localStorage.getItem('data'))
+    obj.forEach((value, index) => {
+        let x = document.createElement("div");
+        let text = value.content;
+        let att2 = value.id;
+        const xl = document.createAttribute('id');
+        xl.value = att2;
+        x.setAttributeNode(xl);
+        x.classList.add('task-list', att2,);
+        document.getElementById("todo-list").appendChild(x);
+        let newDiv = document.createElement("div");
+        newDiv.classList.add(att2, 'check-box');
+        if(value.completed) {
+            newDiv.innerText = ""
+        }else{
+            newDiv.innerText = "X"
+        }
+        newDiv.setAttribute('onclick', 'updatelist('+index+')');
+        const divID = document.createAttribute('id');
+        let dBTN = document.createElement('div');
+        dBTN.classList.add(att2, 'delete-button');
+        dBTN.innerText = 'X';
+        dBTN.setAttribute('onclick', 'deleteRow("' + index + '")');
+        let newTaskItem = document.createElement('div');
+        newTaskItem.classList.add(att2, 'task-text');
+        newTaskItem.innerText = text;
+        if (value.completed) {
+            newTaskItem.classList.add('strike');
+        }
+        x.appendChild(newDiv);
+        x.appendChild(newTaskItem);
+        x.appendChild(dBTN);
+    });
+
+}
+
+export function strike() {
+    
+}
+
+export function updateView () {
+    const parent = document.getElementById("todo-list");
+    removeAllChildNodes(parent);
+    createList1();    
+}
