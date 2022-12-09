de1b0356ce5cc2df1428f3254c494fae4b0cd7dc
Preston Bateman <prestonbbateman@gmail.com>
2022-12-08 23:48:53 -0700

    recent search correction

--- mailtracker/js/testmain.js
+++ mailtracker/js/testmain.js
@@ -43,8 +43,24 @@ function previousSearchFill (array1) {
     obj = document.getElementById('previouslist');
     for (var i = 0; i < array1.length; i++) {
         const node = document.createElement('li');
-        node.innerText = array1[i].number;
-        console.log(node.value);
+        const link = document.createElement('a');
+        link.setAttribute('href', "#");
+        const carrier = array1[i].carrier;
+        const info = array1[i].number;
+        link.innerText = info;
+        link.addEventListener('click', packageDetails.bind(null, carrier, info), false);
+        node.appendChild(link);
         document.getElementById('previouslist').appendChild(node);
     }
+}
+
+function packageDetails (carrier, trackingNumber) {
+    console.log(carrier);
+    const carrierNode = document.createElement('li');
+    const idNode = document.createElement('li');
+    carrierNode.innerText = 'Carrier: ' + carrier.toUpperCase();
+    idNode.innerText = 'Tracking Number: ' + trackingNumber;
+    obj = document.getElementById('trackdetails');
+    obj.appendChild(carrierNode);
+    obj.appendChild(idNode);
 }
